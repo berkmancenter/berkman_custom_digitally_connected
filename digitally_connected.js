9193a4e69f3727d271d242b14f3d27eb5fc6d636
Justin Clark <jclark@cyber.law.harvard.edu>
2014-03-14 14:40:53 -0400

    prevent bio text flash

--- digitally_connected.js
+++ digitally_connected.js
@@ -3,10 +3,15 @@ jQuery(function() {
     container: '#film_roll',
   });
 
+  jQuery('.person-bio p').append('<a href="javascript:void(0)" class="toggle-more">&rarr;</a>');
+
   jQuery(document).on('click', '.toggle-more', function() {
-    jQuery(this).closest('.person').css('height', 'auto').find('p').trigger("originalContent", function( content ) {
-      jQuery(this).replaceWith(content);
-    });
+    jQuery(this).closest('.person').css('height', 'auto').find('p')
+      .trigger("originalContent", function( content ) {
+        // Remove link from end
+        jQuery(this).replaceWith(content.slice(0, -1));
+      }
+    );
   });
 
   // Grab the current date
@@ -34,9 +39,7 @@ jQuery(function() {
 
 jQuery(window).load(function() {
   jQuery('.person-bio p').dotdotdot({
-    height: 130,
-    callback: function() {
-      jQuery(this).append('<a href="javascript:void(0)" class="toggle-more">&rarr;</a>');
-    }
+    height: 165,
+    after: '.toggle-more',
   });
 });
